529d05b474b0c94216a88667bd6eace79f6790d5
arist0v <arist0v@disroot.org>
2022-03-29 15:54:03 -0400

    jetpack learning

--- app/src/main/java/io/webt/webthingsdashboard/MainActivity.kt
+++ app/src/main/java/io/webt/webthingsdashboard/MainActivity.kt
@@ -3,12 +3,15 @@ package io.webt.webthingsdashboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.* //gui component(button, etc...)
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import io.webt.webthingsdashboard.ui.theme.WebthingsDashboardTheme
 
@@ -18,26 +21,84 @@ class MainActivity : ComponentActivity() {
         setContent {
             WebthingsDashboardTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
+        }
+    }
+}
+@Composable
+fun OnBoardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text("Welcome to the Basics Codelab")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+
+    }
+}
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnBoardingPreview() {
+    WebthingsDashboardTheme {
+        OnBoardingScreen(onContinueClicked= {})
+    }
+}
+@Composable
+private fun MyApp(){
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
+    if (shouldShowOnBoarding){
+        OnBoardingScreen(onContinueClicked = { shouldShowOnBoarding = false})
+    }else {
+        Greetings()
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+       items(items = names){ name ->
+           Greeting(name = name)
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val expended = remember { mutableStateOf(false) }
+    val extraPadding = if(expended.value) 48.dp else 0.dp
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical= 4.dp, horizontal = 8.dp)){
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
+
+                Text(text = "Hello,")
+                Text(text = "$name!")
+            }
+            OutlinedButton(
+                onClick = { expended.value = !expended.value }
+            ) {
+                Text(if (expended.value) "Show Less" else "Show More")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     WebthingsDashboardTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
