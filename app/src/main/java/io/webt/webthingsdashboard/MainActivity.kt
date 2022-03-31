eca042415dda6a8018a46adc9358a47ebfe81397
arist0v <arist0v@disroot.org>
2022-03-31 14:41:21 -0400

    working on topappbar

--- app/src/main/java/io/webt/webthingsdashboard/MainActivity.kt
+++ app/src/main/java/io/webt/webthingsdashboard/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.res.stringResource
 import io.webt.webthingsdashboard.ui.theme.WebthingsDashboardTheme
@@ -25,20 +26,25 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun MainApp(){
     /* TODO */
+    var goGatewaySetting by rememberSaveable { mutableStateOf(false) }
     Scaffold(
-        topBar = { TopBar() },
+        topBar = { TopBar( onGatewaySettingsClicked = { goGatewaySetting = true }) },
         content = {
-            GatewaySettings()
+            if(goGatewaySetting) {
+                GatewaySettings()
+            }else{
+                /* TODO */
+            }
         })
 }
 
 @Composable
-private fun TopBar(){
+private fun TopBar(onGatewaySettingsClicked: () -> Unit){
     TopAppBar(
         title = { Text(text = stringResource(id = R.string.app_name)) },
-        actions = {IconButton(onClick = { /* TODO */ }){
+        actions = {IconButton(onClick = { onGatewaySettingsClicked }){
             Icon(Icons.Filled.Settings, contentDescription = "" )
-        }
+            }
         } )
 }
 
