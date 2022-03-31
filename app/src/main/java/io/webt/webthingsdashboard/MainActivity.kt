5fbc2a63e326114b8e9643213cefc7016489a109
arist0v <arist0v@disroot.org>
2022-03-31 13:54:23 -0400

    working on gui

--- app/src/main/java/io/webt/webthingsdashboard/MainActivity.kt
+++ app/src/main/java/io/webt/webthingsdashboard/MainActivity.kt
@@ -3,16 +3,12 @@ package io.webt.webthingsdashboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.* //gui component(button, etc...)
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
 import io.webt.webthingsdashboard.ui.theme.WebthingsDashboardTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +16,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WebthingsDashboardTheme {
-                // A surface container using the 'background' color from the theme
                 MainApp()
             }
         }
@@ -30,149 +25,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun MainApp(){
     /* TODO */
-    GatewaySettings()
+    Scaffold(
+        topBar = { TopBar() },
+        content = {
+            GatewaySettings()
+        })
 }
 
 @Composable
-private fun GatewaySettings(){
-    //TODO change text to xml string
-    val roundedCorner = 5.dp
-    val itemHeight = 48.dp
-    val textWidth = 80.dp
-    val buttonWidth = 100.dp
-
-    Surface(modifier = Modifier.fillMaxSize()) {
-        LazyColumn(modifier = Modifier.padding(4.dp)) {
-            item {
-                Surface(color = MaterialTheme.colors.primary,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .fillMaxWidth()
-                        .height(itemHeight),
-                    shape = RoundedCornerShape(roundedCorner)) {
-                    Row(verticalAlignment = Alignment.CenterVertically){
-                        Text(
-                            text = "Name",
-                            modifier = Modifier
-                                .padding(start = 2.dp)
-                                .width(textWidth)
-                        )
-                        TextField(value = "", onValueChange = { },
-                            modifier = Modifier.padding(2.dp),
-                            shape = RoundedCornerShape(roundedCorner))
-                    }
-                }
-            }
-            //TODO: is there a way to use a function to create the item???
-            item {
-                Surface(color = MaterialTheme.colors.primary,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .fillMaxWidth()
-                        .height(itemHeight),
-                    shape = RoundedCornerShape(roundedCorner)) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Text(
-                            text = "Address",
-                            modifier = Modifier
-                                .padding(start = 2.dp)
-                                .width(textWidth)
-                        )
-                        TextField(value = "", onValueChange = { },
-                            modifier = Modifier.padding(2.dp),
-                            shape = RoundedCornerShape(roundedCorner))
-                    }
-                }
-            }
-
-            item {
-                Surface(color = MaterialTheme.colors.primary,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .fillMaxWidth()
-                        .height(itemHeight),
-                    shape = RoundedCornerShape(roundedCorner)) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Text(
-                            text = "Port",
-                            modifier = Modifier
-                                .padding(start = 2.dp)
-                                .width(textWidth)
-                        )
-                        TextField(value = "", onValueChange = { },
-                            modifier = Modifier.padding(2.dp),
-                            shape = RoundedCornerShape(roundedCorner))
-                    }
-                }
-            }
-
-            item {
-                Surface(color = MaterialTheme.colors.primary,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .fillMaxWidth()
-                        .height(itemHeight),
-                    shape = RoundedCornerShape(roundedCorner)) {
-                    Row(verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier) {
-                        Text(
-                            text = "Enable SSL",
-                            modifier = Modifier
-                                .padding(start = 2.dp)
-                                .width(textWidth)
-                        )
-                        Switch(checked = true, onCheckedChange = {  },
-                            modifier = Modifier.fillMaxWidth())
-                    }
-                }
-            }
-            item {
-                Surface(color = MaterialTheme.colors.primary,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .fillMaxWidth()
-                        .height(itemHeight),
-                    shape = RoundedCornerShape(roundedCorner)) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Text(
-                            text = "Token",
-                            modifier = Modifier
-                                .padding(start = 2.dp)
-                                .width(75.dp)
-                        )
-                        TextField(value = "", onValueChange = { },
-                            modifier = Modifier.padding(2.dp),
-                            shape = RoundedCornerShape(roundedCorner))
-                    }
-                }
-            }
-            
-            item(){
-                Row (modifier = Modifier.fillMaxWidth()){
-                    Button(onClick = { /*TODO*/ },
-                        modifier = Modifier.width(buttonWidth)) {
-                        Text("Cancel")
-
-                    }
-                    Button(onClick = { /*TODO*/ },
-                        modifier = Modifier.width(buttonWidth)) {
-                        Text("Save")
-                    }
-                }
-            }
-
+private fun TopBar(){
+    TopAppBar(
+        title = { Text(text = stringResource(id = R.string.app_name)) },
+        actions = {IconButton(onClick = { /* TODO */ }){
+            Icon(Icons.Filled.Settings, contentDescription = "" )
         }
-
-    }
-
+        } )
 }
-/*
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun GwSettingsPreview(){
-    GatewaySettings()
-}
-*/
+
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
@@ -180,4 +49,5 @@ fun DefaultPreview() {
         MainApp()
     }
 }
+
 
---
+++ app/src/main/java/io/webt/webthingsdashboard/ui/GatewaySettingUI.kt
@@ -0,0 +1,198 @@
+package io.webt.webthingsdashboard
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun GatewaySettings(){
+    val roundedCorner = 5.dp
+    val itemHeight = 48.dp
+    val buttonWidth = 100.dp
+    val textFieldWidth = 220.dp
+
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            LazyColumn(modifier = Modifier
+                .padding(4.dp)
+                .weight(1f)) {
+                item {
+                    Surface(
+                        color = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .fillMaxWidth()
+                            .height(itemHeight),
+                        shape = RoundedCornerShape(roundedCorner)
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Text(
+                                text = stringResource(id = R.string.gwName),
+                                modifier = Modifier
+                                    .padding(start = 2.dp)
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                            )
+                            TextField(
+                                value = "", onValueChange = { },
+                                modifier = Modifier
+                                    .padding(2.dp)
+                                    .width(textFieldWidth),
+                                shape = RoundedCornerShape(roundedCorner)
+                            )
+                        }
+                    }
+                }
+                //TODO: is there a way to use a function to create the item???
+                item {
+                    Surface(
+                        color = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .fillMaxWidth()
+                            .height(itemHeight),
+                        shape = RoundedCornerShape(roundedCorner)
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Text(
+                                text = stringResource(id = R.string.gwAddress),
+                                modifier = Modifier
+                                    .padding(start = 2.dp)
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                            )
+                            TextField(
+                                value = "", onValueChange = { },
+                                modifier = Modifier
+                                    .padding(2.dp)
+                                    .width(textFieldWidth),
+                                shape = RoundedCornerShape(roundedCorner)
+                            )
+                        }
+                    }
+                }
+
+                item {
+                    Surface(
+                        color = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .fillMaxWidth()
+                            .height(itemHeight),
+                        shape = RoundedCornerShape(roundedCorner)
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Text(
+                                text = stringResource(id = R.string.gwPort),
+                                modifier = Modifier
+                                    .padding(start = 2.dp)
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                            )
+                            TextField(
+                                value = "", onValueChange = { },
+                                modifier = Modifier
+                                    .padding(2.dp)
+                                    .width(textFieldWidth),
+                                shape = RoundedCornerShape(roundedCorner)
+                            )
+                        }
+                    }
+                }
+
+                item {
+                    Surface(
+                        color = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .fillMaxWidth()
+                            .height(itemHeight),
+                        shape = RoundedCornerShape(roundedCorner)
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically) {
+                            Text(
+                                text = stringResource(id = R.string.gwSsl),
+                                modifier = Modifier
+                                    .padding(start = 2.dp)
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                            )
+                            Switch(
+                                checked = true,
+                                onCheckedChange = { },
+                                modifier = Modifier.padding(end = 5.dp)
+                            )
+                        }
+                    }
+                }
+                item {
+                    Surface(
+                        color = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .fillMaxWidth()
+                            .height(itemHeight),
+                        shape = RoundedCornerShape(roundedCorner)
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Text(
+                                text = stringResource(id = R.string.gwToken),
+                                modifier = Modifier
+                                    .padding(start = 2.dp)
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                            )
+                            TextField(
+                                value = "", onValueChange = { },
+                                modifier = Modifier
+                                    .padding(2.dp)
+                                    .width(textFieldWidth),
+                                shape = RoundedCornerShape(roundedCorner)
+                            )
+                        }
+                    }
+                }
+            }
+
+            Surface(
+                modifier = Modifier
+                    .background(MaterialTheme.colors.background)
+            ) {
+                Row(modifier = Modifier.padding(bottom = 10.dp)) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .width(buttonWidth)
+                            .padding(end = 5.dp)
+                    ) {
+                        Text(stringResource(id = R.string.save))
+                    }
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.width(buttonWidth)
+                            .padding(start = 5.dp)
+                    ) {
+                        Text(stringResource(id = R.string.cancel))
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GwSettingsPreview(){
+    GatewaySettings()
+}
